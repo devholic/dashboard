408ddab5a2d3657e8349649dc56940842e038424
devholic <devholic@plusquare.com>
2015-12-26 23:56:35 +0900

    Change StartActivity

--- app/src/main/java/us/taleby/dashboard/StartActivity.java
+++ app/src/main/java/us/taleby/dashboard/StartActivity.java
@@ -1,13 +1,41 @@
 package us.taleby.dashboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+import us.taleby.dashboard.home.Home;
 
 public class StartActivity extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
+        handler = new Handler();
+        runnable = () -> {
+            Intent intent = new Intent(StartActivity.this, Home.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        };
+        handler.postDelayed(runnable, 2000);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(runnable);
+        super.onDestroy();
     }
 }
